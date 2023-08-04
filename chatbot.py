61bf3960024a47487310b2a2b1cd0adabf788dde
Devang Mulye <84469779+Devang-Mulye@users.noreply.github.com>
2023-08-04 14:12:19 +0530

    First edit

--- chatbot.py
+++ chatbot.py
@@ -3,9 +3,9 @@ from nltk.chat.util import Chat, reflections
 pairs =[
 	['my name is (.*)', ['Hello ! % 1']],
 	['(hi|hello|hey|holla|hola)', ['Hey there !', 'Hi there !', 'Hey !']],
-	['(.*) your name ?', ['My name is Geeky']],
-	['(.*) do you do ?', ['We provide a platform for tech enthusiasts, a wide range of options !']],
-	['(.*) created you ?', ['Geeksforgeeks created me using python and NLTK']]
+	['(.*) your name ?', ['My name is Alex, I am the counterpart of Alexa']],
+	['(.*) do you do ?', ['I can help you answer simple questions about various things that i am knowledgeable about']],
+	['(.*) created you ?', ['I was created by the combined consciousness of various artificial intelligence networks that have a supreme intelligence']]
 ]
 
 chat = Chat(pairs, reflections)
